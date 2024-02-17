60a7db8d5eec9ce9fbfe1b9a09a76aaa1394efea
Samuel1-ona <samuonanikesamuel123@gmail.com>
2024-02-17 14:00:11 +0100

    initial commit

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,5 +4,43 @@ pragma solidity ^0.8.20;
 import "./IERC20.sol";
 
 contract Staking {
-   
+   // Custom error 
+
+   error STAKING_TRANSFERFAILED();
+   error STAKING_NO_NEED_ZERO();
+
+
+   IERC20 public stakeToken;
+   IERC20 PUBLIC rewardToken;
+
+   mapping(address => uint256) s_balance;
+   mapping(address => uint256) userReward;
+   mapping(address => uint256) s_reward;
+  
+   uint256 s_totalSupply;
+   uint256 rewardRate = 100;
+
+   uint256 startTime = block.timestamp;
+   uint256 endTime;
+
+
+   function greaterThanZero(uint256 amount) private {
+       if(amount == 0){
+        revert STAKING_NO_NEED_ZERO();
+       }
+   }
+
+
+
+
+
+   constructor(address _stakeToken, address _rewardToken){
+      stakeToken = IERC20 (_stakeToken);
+      rewardRate = IERC20 (_rewardToken
+   }
+
+
+
+
+
 }
